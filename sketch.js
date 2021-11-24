d7d77d13019cbc32864ad55c59c4d333a5f97704
Luis13625283 <90009089+Luis13625283@users.noreply.github.com>
2021-11-24 16:59:32 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,77 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
-var particles = [];
-var plinkos = [];
-var divisions =[];
-var divisionHeight=300;
-var score =0;
-function setup() {
-  createCanvas(800, 800);
-  engine = Engine.create();
-  world = engine.world;
-  ground = new Ground(width/2,height,width,20);
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var thunder, thunder1,thunder2,thunder3,thunder4;
 
-  //crea los objetos para dividir
-  for (var k = 0; k <=80; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
+var engine, world;
+var drops = [];
+var rand;
 
-  //crea la primera fila de objetos plinko
-  for (var j = 75; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
+var maxDrops=100;
 
-  //crea la segunda fila de objetos plinko
-  for (var j = 50; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,175));
-  }
+var thunderCreatedFrame=0;
 
-  //crea la tercera fila de objetos plinko
-  for (var j = 40; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
-  
-  //crea la cuarta fila de objetos plinko
-  for (var j = 15; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
+function preload(){
+    thunder1 = loadImage("thunderbolt/1.png");
+    thunder2 = loadImage("thunderbolt/2.png");
+    thunder3 = loadImage("thunderbolt/3.png");
+    thunder4 = loadImage("thunderbolt/4.png");
+}
 
-  //crea los objetos partícula
-  if(frameCount%60===0){
-    particles.push(new Particle(random(width/2-10, width/2+10), 10,10));
-  }
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(400,700);
+    umbrella = new Umbrella(200,500);
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
     
 }
- 
 
+function draw(){
+    Engine.update(engine);
+    background(0); 
 
-function draw() {
-  background("black");
-  textSize(20)
- 
-  Engine.update(engine);
-  ground.display();
-  
-  //muestra los plinkos 
-  for (var i = 0; i < plinkos.length; i++) {
-    plinkos[i].display();   
-  }
-   
-  //muestra las divisiones
-  for (var k = 0; k < divisions.length; k++) {
-    divisions[k].display();
-  }
+    //creating thunder
+    rand = Math.round(random(1,4));
+    if(frameCount%80===0){
+        thunderCreatedFrame=frameCount;
+        thunder = createSprite(random(10,370), random(10,30), 10, 10);
+        switch(rand){
+            case 1: thunder.addImage(thunder1);
+            break;
+            case 2: thunder.addImage(thunder2);
+            break; 
+            case 3: thunder.addImage(thunder3);
+            break;
+            case 4: thunder.addImage(thunder4);
+            break;
+            default: break;
+        }
+        thunder.scale = random(0.3,0.6)
+    }
 
-  //muestra las partículas 
-  for (var j = 0; j < particles.length; j++) {
-    particles[j].display();
-  }
-}
+    if(thunderCreatedFrame + 10 ===frameCount && thunder){
+        thunder.destroy();
+    }
+
+    umbrella.display();
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+        
+    }
+
+    drawSprites();
+}   
+
